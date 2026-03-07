758c52ed266ef63616dfe255dcce9b6a9f422276
Ahmed Besar <besarahmed89@gmail.com>
2026-03-07 17:31:43 +0200

    remove TotalPrice from ShoppingCart and add it in ShoppingCartResponseDto

--- services/Basket/Basket.Application/Responses/ShoppingCartResponseDto.cs
+++ services/Basket/Basket.Application/Responses/ShoppingCartResponseDto.cs
@@ -4,5 +4,5 @@ public sealed record ShoppingCartResponseDto
 {
     public string UserName { get; set; } = default!;
     public List<ShoppingCartItemResponseDto> Items { get; set; } = new();
-    public decimal TotalPrice { get; set; }
+    public decimal TotalPrice => Items.Sum(item => item.Price * item.Quantity);
 }
